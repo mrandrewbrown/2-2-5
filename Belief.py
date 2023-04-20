f70d10d11e96bef1d2bf412854cfa70cdcafe49a
mrandrewbrown <54468856+mrandrewbrown@users.noreply.github.com>
2023-04-20 02:09:33 +0100

    Update Belief.py

--- Belief.py
+++ Belief.py
@@ -1,47 +1,29 @@
 
+
 def reverse_inquiry(belief):
     return "I am " + "not "*(belief.endswith("enough")) + "enough"
+                    
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        belief = request.form['belief']
-        reverse_belief = reverse_inquiry(belief)
-        return f"Original belief: {belief}<br>Flipped belief: {reverse_belief}"
+belief = input("Enter the belief or thought that causes you distress or discomfort: ")
+reverse_belief = reverse_inquiry(belief)
+
+
+input("Now, Pull up VIPs.  Reverse the belief and say it out loud to yourself and press Enter to continue...")
+
+# Wait for the person to press Enter before continuing
+input("Sit quietly and observe your thoughts and feelings as they arise in response to the original belief or thought. Press Enter to continue when you're ready...")
+
+# Loop until the person notices a shift in their perception of themselves
+while True:
+    # Ask the person to observe their thoughts and feelings without judgment or resistance
+    input("Continue observing and witnessing your thoughts and feelings as they arise. Press Enter when you're ready to move on...")
+
+    # Check if the person noticed a shift in their perception of themselves
+    answer = input("Have you noticed a shift in your perception of yourself? (y/n) ")
+    if answer.lower() == "y":
+        print("Congratulations!")
+        break
+    elif answer.lower() == "n":
+        continue
     else:
-        return '''
-            <form method="post">
-                <label for="belief">Enter the belief or thought that causes you distress or discomfort:</label>
-                <input type="text" id="belief" name="belief">
-                <br>
-                <input type="submit" value="Flip Belief">
-            </form>
-        '''
-
-@app.route('/observe')
-def observe():
-    return '''
-        <form method="post" action="/observe">
-            <p>Continue observing and witnessing your thoughts and feelings as they arise. Press Enter when you're ready to move on...</p>
-            <input type="submit" value="Next">
-        </form>
-    '''
-
-@app.route('/shift', methods=['POST'])
-def shift():
-    answer = request.form['answer']
-    if answer.lower() == 'y':
-        return "Congratulations!"
-    elif answer.lower() == 'n':
-        return '''
-            <form method="post" action="/observe">
-                <p>Have you noticed a shift in your perception of yourself? (y/n)</p>
-                <input type="text" id="answer" name="answer">
-                <input type="submit" value="Submit">
-            </form>
-        '''
-    else:
-        return "Invalid input. Please enter 'y' or 'n'."
-
-if __name__ == '__main__':
-    app.run()
+        print("Invalid input. Please enter 'y' or 'n'.")
